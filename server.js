650b3926691a5dbc181e6b22594b5e32b6ca6937
Anshul Karyal <karyalanshul@gmail.com>
2025-01-29 18:05:37 +0530

    sdfgh

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*", // Change this to your frontend URL in production
+    origin: "https://videofrontend-eta.vercel.app", // Change this to your frontend URL in production
     methods: ["GET", "POST"],
   },
 });
